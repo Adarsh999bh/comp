c96d199645b9cd571d7016cbea20311f1327b046
Adarsh999bh <adarshbhandary48@gmail.com>
2021-08-19 12:05:47 +0530

    attendance check code added

--- src/main/java/line/jav/Empwage.java
+++ src/main/java/line/jav/Empwage.java
@@ -1,8 +1,22 @@
 package line.jav;
+import java.util.Scanner;
 
 public class Empwage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program.!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of employees");
+        int n=sc.nextInt();
+        sc.close();
+        for (int i=0;i<n;i++){
+            int status=(int) (Math.floor(Math.random()*10)%2);
+            if(status==0){
+                System.out.println("Employee "+(i+1)+" is Absent");
+            }
+            else{
+                System.out.println("Employee "+(i+1)+" is Present");
+            }
+        }
     }
     
 }
