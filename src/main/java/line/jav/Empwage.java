9f690cc644030c67dfcd33cb71686ef2aec76f9c
Adarsh999bh <adarshbhandary48@gmail.com>
2021-08-19 13:11:47 +0530

    monthly wage calculation is added

--- src/main/java/line/jav/Empwage.java
+++ src/main/java/line/jav/Empwage.java
@@ -9,19 +9,27 @@ public class Empwage {
         int n=sc.nextInt();
         sc.close();
         int pt_wage=0,ft_wage=0;
-        for (int i=0;i<n;i++){
-            int status=(int) (Math.floor(Math.random()*10)%3);
-            if(status==0){
-                System.out.println("Employee "+(i+1)+" is Absent");
+        int ep_wage[]=new int[n];
+        for(int j=0;j<20;j++){
+            for (int i=0;i<n;i++){
+                int status=(int) (Math.floor(Math.random()*10)%3);
+                if(status==0){
+                    System.out.println("Employee "+(i+1)+" is Absent on day "+j);
+                }
+                else if (status==1){
+                    System.out.println("Employee "+(i+1)+" is Present (Part-time) on day "+j);
+                    pt_wage+=80;
+                    ep_wage[i]+=80;
+                }
+                else{
+                    System.out.println("Employee "+(i+1)+" is Present (Full-time) on day "+j);
+                    ft_wage+=160;
+                    ep_wage[i]=160;
+                }
             }
-            else if (status==1){
-                System.out.println("Employee "+(i+1)+" is Present (Part-time)");
-                pt_wage+=20*4;
-            }
-            else{
-                System.out.println("Employee "+(i+1)+" is Present (Full-time)");
-                ft_wage+=20*8;
-            }
+        }
+        for(int i=0;i<n;i++){
+            System.out.println("monthly wage for Employee "+(i+1)+" is "+ep_wage[i]);
         }
         System.out.println("total cost for company on part-time employee is "+pt_wage);
         System.out.println("total cost for company on full-time employee is "+ft_wage);
