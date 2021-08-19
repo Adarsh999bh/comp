f6ac511ad6f268a850518135c0648d1697ff8d4f
Adarsh999bh <adarshbhandary48@gmail.com>
2021-08-19 08:13:27 +0530

    distance calculation code added

--- src/main/java/line/jav/App.java
+++ src/main/java/line/jav/App.java
@@ -1,6 +1,6 @@
 package line.jav;
 
-/**
+import java.util.Scanner;/**
  * Hello world!
  *
  */
@@ -9,5 +9,31 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to line comp" );
-    }
+        int l1[]=new int[4];
+        int l2[]=new int[4];
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the coordinates for line 1");
+        System.out.print("X_1=");
+        l1[0]=sc.nextInt();
+        System.out.print("Y_1=");
+        l1[1]=sc.nextInt();
+        System.out.print("X_2=");
+        l1[2]=sc.nextInt();
+        System.out.print("Y_2=");
+        l1[3]=sc.nextInt();
+        System.out.print("enter the coordinates for line 2\n");
+        System.out.print("X_1=");
+        l2[0]=sc.nextInt();
+        System.out.print("Y_1=");
+        l2[1]=sc.nextInt();
+        System.out.print("X_2=");
+        l2[2]=sc.nextInt();
+        System.out.print("Y_2=");
+        l2[3]=sc.nextInt();
+        sc.close();
+        Double dis1=new Double(Math.sqrt(Math.pow(l1[3]-l1[1],2)+Math.pow(l1[2]-l1[0],2)));
+        Double dis2=new Double(Math.sqrt(Math.pow(l2[3]-l2[1],2)+Math.pow(l2[2]-l2[0],2)));
+        System.out.println("length of line 1 = "+dis1+"\nlength of line 2 = "+dis2);
+
+}
 }
